094584ead2e6fec8933495c49c7c6796e737ddfc
Cassio Santana <cassioo.roger@gmail.com>
2023-06-23 09:45:50 -0300

    update action create in subscriptions_controller.rb

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,5 +5,15 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
+    customer = current_user.stripe_customer
+    begin
+      subscription = customer.subscriptions.create(source: params[:stripeToken], plan: 'pro')
+      current_user.assign_attributes(stripe_subscription_id: subscription.id)
+      current_user.save
+      redirect_to root_path, notice: 'Thanks for subscribing!'
+    rescue Stripe::CardError => e
+      flash.alert = e.message
+      render action: :new
+    end
   end
 end
