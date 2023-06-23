8fee65d15924d29d2a6b8669d98f2bd1e091396a
Cassio Santana <cassioo.roger@gmail.com>
2023-06-23 09:28:18 -0300

    add subscriptions routes new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :subscriptions, only: [:new, :create]
   root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
