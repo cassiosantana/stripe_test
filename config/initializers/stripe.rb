d8873e61e33cc559b90ccabd967e055bd07393bc
Cassio Santana <cassioo.roger@gmail.com>
2023-06-23 16:31:53 -0300

    update stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,10 @@
-Stripe.api_key = Rails.application.credentials.stripe_private_key
+# Stripe.api_key = Rails.application.credentials.stripe_private_key
+
+Stripe.api_key = ENV['STRIPE_API_KEY']
+StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET']
+
+StripeEvent.configure do |config|
+  config.subscribe 'checkout.session.completed' do |event|
+    PaymentReceived.call(event)
+  end
+end
