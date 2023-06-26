0197cd65fdccad1e116df4ad290ee7224f2ce4e7
Cassio Santana <cassioo.roger@gmail.com>
2023-06-26 13:58:18 -0300

    update credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
-# Stripe.api_key = Rails.application.credentials.stripe_private_key
+Stripe.api_key = Rails.application.credentials.stripe_private_key
 
-Stripe.api_key = ENV['STRIPE_API_KEY']
+# Stripe.api_key = ENV['STRIPE_API_KEY']
 StripeEvent.signing_secret = ENV['STRIPE_SIGNING_SECRET']
 
 StripeEvent.configure do |config|
