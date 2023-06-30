0cf87e425a1fe3bc05fb422fa5ba8ae24867632a
Cassio Santana <cassioo.roger@gmail.com>
2023-06-30 17:01:43 -0300

    add payment method in customer creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
 
   def stripe_customer
     return Stripe::Customer.retrieve(stripe_id) if stripe_id?
-    stripe_customer = Stripe::Customer.create(email: email)
+    stripe_customer = Stripe::Customer.create(email: email,
+    payment_method: 'pm_card_visa',
+      invoice_settings: {
+      default_payment_method: 'pm_card_visa'
+    })
     update(stripe_id: stripe_customer.id)
     stripe_customer
   end
