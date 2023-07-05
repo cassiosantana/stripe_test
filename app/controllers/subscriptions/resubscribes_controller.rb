f97788d60db0e029f2ed104aa3644fcb0bfa0bb5
Cassio Santana <cassioo.roger@gmail.com>
2023-07-05 10:45:13 -0300

    build(subscriptions): add resubscribes_controller.rb

---
+++ app/controllers/subscriptions/resubscribes_controller.rb
@@ -0,0 +1,16 @@
+# controllers/subscriptions/resubscribes_controller.rb
+module Subscriptions
+  class ResubscribesController < ApplicationController
+
+    def new
+      subscription = current_user.stripe_customer.subscriptions.first
+      if subscription.save
+        current_user.update(stripe_subscription_id: subscription.id, expires_at: nil)
+        flash.notice = "Thanks for resubscribing!"
+      else
+        flash.alert = "There was a problem resubscribing!"
+      end
+      redirect_to root_path
+    end
+  end
+end
