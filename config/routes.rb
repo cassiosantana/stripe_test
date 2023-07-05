f5b7f95f1cdc8261dd257e569464ab8335ee41cc
Cassio Santana <cassioo.roger@gmail.com>
2023-07-05 09:29:27 -0300

    build(subscriptions): add routes to unsubscribe and resubscribe

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   mount StripeEvent::Engine, at: '/stripe/webhook'
   resources :products
   devise_for :users
-  resources :subscriptions, only: [:new, :create]
+  resources :subscriptions, only: [:new, :create] do
+    scope module: 'subscriptions' do
+      collection do
+        resource :unsubscribe, only: :destroy
+        resource :resubscribe, only: :new
+      end
+    end
+  end
   root to: 'home#index'
   get '/example', to: 'home#example'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
