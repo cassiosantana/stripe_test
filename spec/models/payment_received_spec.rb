6c2d44c1796d204d2ef21ab5bc167379378d9a72
Cassio Santana <cassioo.roger@gmail.com>
2023-07-07 18:51:57 -0300

    test(payment): should credit user vbucks

--- spec/models/payment_received_spec.rb
+++ spec/models/payment_received_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PaymentReceived, type: :model do
+  let!(:product) { Product.create(name: "10 vbucks", price: 9.99, quantity: 10) }
   let(:user) { User.create(email: "teste@example.com", password: "pass_123") }
 
   before do
@@ -20,10 +21,17 @@ RSpec.describe PaymentReceived, type: :model do
   end
 
   it "should credit user vbucks" do
-    allow_any_instance_of(PaymentReceived).to receive(:line_items).and_return([])
+    expect(user.vbuck.to_i).to eq(0)
+    allow_any_instance_of(PaymentReceived).to receive(:line_items).and_return(
+      [
+         OpenStruct.new(description: '10 vbucks', quantity: 1),
+         OpenStruct.new(description: '10 vbucks', quantity: 1)
+      ]
+    )
     payment_received =  PaymentReceived.new(@payment).call
     user.reload
     expect(user.stripe_id).to eq("cus_test")
     expect(payment_received).to eq(true)
+    expect(user.vbuck).to eq(20)
   end
 end
