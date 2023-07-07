76ffaca7edba28a925f23309632b265b96923da4
Cassio Santana <cassioo.roger@gmail.com>
2023-07-07 12:47:00 -0300

    build(test): adds base code of the Payment Received test

--- spec/models/payment_received_spec.rb
+++ spec/models/payment_received_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe PaymentReceived, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(email: "teste@example.com", password: "pass_123") }
+
+  before do
+    json = {
+      data: {
+        object: {
+          id: "cs_test_id",
+          amount_subtotal: 399,
+          amount_total: 399,
+          client_reference_id: user.id,
+          customer: "cus_test",
+        }
+      }
+    }.to_json
+
+    @payment = JSON.parse(json, object_class: OpenStruct)
+  end
+
+  it "should credit user vbucks" do
+    payment_received = PaymentReceivedMock.call(@payment)
+    user.reload
+    expect(user.stripe_id).to eq("cus_test")
+    expect(payment_received).to be_truthy
+  end
 end
