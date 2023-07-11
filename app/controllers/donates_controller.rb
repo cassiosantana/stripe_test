ce526e50ba62e87d7c5811bd31dcb7e1fe3e1275
Cassio Santana <cassioo.roger@gmail.com>
2023-07-11 14:06:00 -0300

    build(donation): create a checkout session and redirection to session.url

--- app/controllers/donates_controller.rb
+++ app/controllers/donates_controller.rb
@@ -1,5 +1,27 @@
 class DonatesController < ApplicationController
   def create
+    session = Stripe::Checkout::Session.create(
+      billing_address_collection: :auto,
+      mode: :payment,
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            unit_amount: params[:amount],
+            currency: "USD",
+            product_data: {
+              name: "Donation",
+              description: "donation"
+            },
+          },
+          quantity: 1,
+        }
+      ],
+      success_url: "http://localhost:3000/donates/thankyou",
+      cancel_url: "http://localhost:3000"
+    )
+
+    redirect_to session.url, allow_other_host: true
   end
 
   def thankyou
