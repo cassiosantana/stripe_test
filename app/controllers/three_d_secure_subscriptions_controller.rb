88813c29045decacc3ac6034001e8b19f39f7764
Cassio Santana <cassioo.roger@gmail.com>
2023-07-12 17:41:59 -0300

    build(3D Secure): add session in action new
    Code will likely change due to changes in how subscriptions are created. Code for registration only.
    
    Ref #5

--- app/controllers/three_d_secure_subscriptions_controller.rb
+++ app/controllers/three_d_secure_subscriptions_controller.rb
@@ -1,5 +1,17 @@
 class ThreeDSecureSubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      client_reference_id: current_user.id,
+      customer_email: current_user.email,
+      subscription_data: {
+        items: [{
+          plan: 'pro'
+        }]
+      }
+    )
   end
 
   def create
