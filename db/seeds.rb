e416052c948c6ce5e905d44bc165831dfeaa3e0c
Cassio Santana <cassioo.roger@gmail.com>
2023-07-13 10:30:03 -0300

    build(3D Secure): add creating a premium plan to seeds.rb
    Ref #5

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Stripe::Plan.create(product: { name: 'Pro', id: 'pro'},
-                    interval: 'month',
-                    currency: 'usd',
-                    amount: 1500)
+# Stripe::Plan.create(product: { name: 'Pro', id: 'pro'},
+#                     interval: 'month',
+#                     currency: 'usd',
+#                     amount: 1500)
+
+begin
+  Stripe::Plan.create(
+    product: { name: "Premium", id: "premium" },
+    id: "premium",
+    interval: "month",
+    billing_scheme: "per_unit",
+    currency: "usd",
+    amount: "5000"
+  )
+rescue Stripe::InvalidRequestError => e
+  puts "Premium plan failed to create: #{e.message}"
+end
