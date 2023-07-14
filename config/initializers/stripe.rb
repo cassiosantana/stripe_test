ce31f92433422ce5126ce6193cfc950eaac9d618
Cassio Santana <cassioo.roger@gmail.com>
2023-07-14 13:49:13 -0300

    fix(3D Secure): changes event handling according to event mode
    stripe.rb
    
    Ref #5

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,12 +4,10 @@ StripeEvent.signing_secret = ENV['STRIPE_WEBHOOK_KEY']
 
 StripeEvent.configure do |config|
   config.subscribe 'checkout.session.completed' do |event|
-    PaymentReceived.call(event)
-  end
-end
-
-StripeEvent.configure do |config|
-  config.subscribe 'checkout.session.completed' do |event|
-    SubscribeService.call(event)
+    if event.data.object.mode == "payment"
+      PaymentReceived.call(event)
+    elsif event.data.object.mode == "subscription"
+      SubscribeService.call(event)
+    end
   end
 end
