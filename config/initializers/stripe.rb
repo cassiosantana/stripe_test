d9895b3c235e0d9141df0ebc7f191165d4af1e0a
Cassio Santana <cassioo.roger@gmail.com>
2023-07-14 09:16:19 -0300

    build: updates the project to use environment variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,6 @@
-Stripe.api_key = Rails.application.credentials.stripe_private_key
+Stripe.api_key = ENV['STRIPE_API_KEY']
 
-# Stripe.api_key = ENV['STRIPE_API_KEY']
-StripeEvent.signing_secret = Rails.application.credentials.signing_secret
+StripeEvent.signing_secret = ENV['STRIPE_WEBHOOK_KEY']
 
 StripeEvent.configure do |config|
   config.subscribe 'checkout.session.completed' do |event|
