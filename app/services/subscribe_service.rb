574980c1826382e10b3999816ec491f9763a1795
Cassio Santana <cassioo.roger@gmail.com>
2023-07-14 11:14:07 -0300

    build(3D Secure): update user stripe attributes in call method
    subscribe_service.rb
    
    Ref #5

--- app/services/subscribe_service.rb
+++ app/services/subscribe_service.rb
@@ -9,6 +9,15 @@ class SubscribeService
   end
 
   def call
-    # TODO
+    user = User.find_by(id: object.client_reference_id)
+    return unless user
+
+    user.update(stripe_id: object.customer, stripe_subscription_id: object.subscription)
+  end
+
+  private
+
+  def object
+    @object ||= event.data.object
   end
 end
